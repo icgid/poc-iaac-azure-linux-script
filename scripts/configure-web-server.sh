c400261938f9728c5bd98ac3f096aa07f68c83b7
Agus Suhanto <agus.suhanto@gmail.com>
2018-08-22 17:01:40 +0700

    Wordpress downloaded

--- scripts/configure-web-server.sh
+++ scripts/configure-web-server.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# install dotnet core
+# Donwload wordpress from wordpress.org, and copy it to /opt/wordpress
+sudo wget https://wordpress.org/latest.tar.gz /
+sudo tar -xvzf latest.tar.gz -C /opt
+
+# Install nginx
+sudo apt-get install -y nginx
+sudo service nginx start
+
+# Install dotnet core
 wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
-sudo ap-get install apt-transport-https
+sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install dotnet-sdk-2.1.4 -y
