95acac1b400899ef6708246169051c56ffac488d
Agus Suhanto <agus.suhanto@gmail.com>
2018-08-22 20:44:27 +0700

    Nginx config added

--- scripts/configure-web-server.sh
+++ scripts/configure-web-server.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Donwload wordpress from wordpress.org, and copy it to /opt/wordpress
-sudo wget https://wordpress.org/latest.tar.gz /
-sudo tar -xvzf latest.tar.gz -C /opt
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar -xvzf latest.tar.gz
+sudo mkdir /opt/www
+sudo cp -R wordpress/* /opt/www/
 
 # Install nginx
+sudo apt-get update
 sudo apt-get install -y nginx
 sudo service nginx start
+sudo touch /etc/nginx/sites-available/default
+sudo wget https://raw.githubusercontent.com/icgid/poc-iac-azure-linux-script/master/configs/nginx/default
+sudo cp default /etc/nginx/sites-available/
 
 # Install dotnet core
 wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
