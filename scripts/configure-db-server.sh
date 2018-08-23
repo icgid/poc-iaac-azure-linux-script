f87511037e4aac6f25d3382fe3242abfd4adbef4
Agus Suhanto <agus.suhanto@gmail.com>
2018-08-23 17:00:15 +0700

    Debugging configure-db-server.sh

--- scripts/configure-db-server.sh
+++ scripts/configure-db-server.sh
@@ -7,6 +7,6 @@ sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again p
 sudo apt-get install -y mysql-server
 
 # Create wordpress database
-#sudo wget https://raw.githubusercontent.com/icgid/poc-iac-azure-linux-script/master/scripts/create-db.sql
-#sudo sed -i "s/<userpassword>/$2/g" create-db.sql
-#sudo mysql -u"root" -p"$1" < create-db.sql
+sudo wget https://raw.githubusercontent.com/icgid/poc-iac-azure-linux-script/master/scripts/create-db.sql
+sudo sed -i "s/<dbuserpassword>/$2/g" create-db.sql
+sudo mysql -u"root" -p"$1" < create-db.sql
