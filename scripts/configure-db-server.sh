58564fb3055654285cdb921fc77f2e9831abdf8d
Agus Suhanto <agus.suhanto@gmail.com>
2018-08-23 14:58:46 +0700

    Fixing bug in azuredeploy.json

--- scripts/configure-db-server.sh
+++ scripts/configure-db-server.sh
@@ -2,7 +2,7 @@
 
 # Configure mysql-server
 sudo apt-get update
-sudo apt-get install debconf-utils
+sudo apt-get install -y debconf-utils
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $1"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $1"
 sudo apt-get install -y mysql-server
