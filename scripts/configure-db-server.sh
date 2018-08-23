69c4a74427f4b45474e333f068da106967661724
Agus Suhanto <agus.suhanto@gmail.com>
2018-08-23 19:15:36 +0700

    Change mysql bind_address

--- scripts/configure-db-server.sh
+++ scripts/configure-db-server.sh
@@ -7,6 +7,8 @@ export DEBIAN_FRONTEND="noninteractive"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $1"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $1"
 sudo apt-get install -y mysql-server
+sudo sed -i "s/= 127.0.0.1/= 0.0.0.0/g" /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo systemctl restart mysql
 
 # Create wordpress database
 sudo wget https://raw.githubusercontent.com/icgid/poc-iac-azure-linux-script/master/scripts/create-db.sql
