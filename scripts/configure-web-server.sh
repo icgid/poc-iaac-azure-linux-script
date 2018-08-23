5fb4c8cbbdb28473e849ea902e07e713a6c06b44
Agus Suhanto <agus.suhanto@gmail.com>
2018-08-23 14:11:04 +0700

    Commenting install dotnet core section

--- scripts/configure-web-server.sh
+++ scripts/configure-web-server.sh
@@ -16,8 +16,8 @@ sudo cp default /etc/nginx/sites-available/
 sudo nginx -s reload
 
 # Install dotnet core
-wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install dotnet-sdk-2.1.4 -y
+#wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+#sudo dpkg -i packages-microsoft-prod.deb
+#sudo apt-get install apt-transport-https
+#sudo apt-get update
+#sudo apt-get install dotnet-sdk-2.1.4 -y
