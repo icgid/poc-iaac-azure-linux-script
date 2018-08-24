c38123ce8d888bfb585aa4285085b514f39bcb34
Agus Suhanto <agus.suhanto@gmail.com>
2018-08-24 14:47:43 +0700

    Using wordpress SQL dump

--- scripts/configure-db-server.sh
+++ scripts/configure-db-server.sh
@@ -11,6 +11,10 @@ sudo sed -i "s/= 127.0.0.1/= 0.0.0.0/g" /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo systemctl restart mysql
 
 # Create wordpress database
-sudo wget https://raw.githubusercontent.com/icgid/poc-iac-azure-linux-script/master/scripts/create-db.sql
+sudo wget https://raw.githubusercontent.com/icgid/poc-iaac-azure-linux-script/master/scripts/create-db.sql
 sudo sed -i "s/<dbuserpassword>/$2/g" create-db.sql
 sudo mysql -u"root" -p"$1" < create-db.sql
+
+# Restore wordpress database
+sudo wget https://raw.githubusercontent.com/icgid/poc-iaac-azure-linux-script/master/scripts/wordpress.demo.sql
+sudo mysqldump -u"root" -p"$1" wordpress < wordpress.demo.sql
--- scripts/configure-web-server.sh
+++ scripts/configure-web-server.sh
@@ -3,11 +3,11 @@
 sudo apt-get update
 
 # Donwload wordpress from wordpress.org, and copy it to /opt/wordpress
-sudo wget https://wordpress.org/latest.tar.gz
-sudo tar -xvzf latest.tar.gz
-sudo mkdir -p /var/wordpress/html
-sudo cp -R wordpress/* /var/wordpress/html/
-sudo chown -R www-data:www-data /var/wordpress/html/
+sudo wget https://wordpress.org/wordpress-4.9.8.tar.gz
+sudo tar -xvzf wordpress-4.9.8.tar.gz
+sudo mkdir -p /var/www/wordpress
+sudo cp -R wordpress/* /var/www/wordpress/
+sudo chown -R www-data:www-data /var/www/wordpress/
 
 # Install nginx
 sudo apt-get install -y nginx
@@ -15,9 +15,15 @@ sudo apt-get install -y php-fpm php-mysql
 sudo sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php/7.0/fpm/php.ini
 sudo service nginx start
 sudo systemctl restart php7.0-fpm
-sudo wget https://raw.githubusercontent.com/icgid/poc-iac-azure-linux-script/master/configs/nginx/wordpress -O /etc/nginx/sites-available/wordpress
+sudo wget https://raw.githubusercontent.com/icgid/poc-iaac-azure-linux-script/master/configs/nginx/wordpress -O /etc/nginx/sites-available/wordpress
+sudo sed -i "s/<wordpressDomainName>/$1/g" /etc/nginx/sites-available/wordpress
 sudo ln -s /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/
 sudo systemctl reload nginx
+
+sudo wget https://raw.githubusercontent.com/icgid/poc-iaac-azure-linux-script/master/configs/wordpress/wp-config.php -O /var/www/wordpress/wp-config.php
+sudo sed -i "s/<dbUserPassword>/$2/g" /var/www/wordpress/wp-config.php
+sudo sed -i "s/<backendIPAddress>/$3/g" /var/www/wordpress/wp-config.php
+sudo chown www-data:www-data /var/www/wordpress/wp-config.php
 
 # Install dotnet core
 #wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
