5ebebdb5bec1a46ac69cf6a4181080a317f7985d
Agus Suhanto <agus.suhanto@gmail.com>
2018-09-03 13:30:38 +0700

    Fix src path

--- src/scripts/configure-web-server.sh
+++ src/scripts/configure-web-server.sh
@@ -15,13 +15,13 @@ sudo apt-get install -y php-fpm php-mysql
 sudo sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php/7.0/fpm/php.ini
 sudo service nginx start
 sudo systemctl restart php7.0-fpm
-sudo wget https://raw.githubusercontent.com/icgid/poc-iaac-azure-linux-script/master/configs/nginx/wordpress -O /etc/nginx/sites-available/wordpress
+sudo wget https://raw.githubusercontent.com/icgid/poc-iaac-azure-linux-script/master/src/configs/nginx/wordpress -O /etc/nginx/sites-available/wordpress
 sudo sed -i "s/<wordpressDomainName>/$1/g" /etc/nginx/sites-available/wordpress
 sudo ln -s /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/
 sudo systemctl reload nginx
 
 # Upload existing wordpress configuration file
-sudo wget https://raw.githubusercontent.com/icgid/poc-iaac-azure-linux-script/master/configs/wordpress/wp-config.php -O /var/www/wordpress/wp-config.php
+sudo wget https://raw.githubusercontent.com/icgid/poc-iaac-azure-linux-script/master/src/configs/wordpress/wp-config.php -O /var/www/wordpress/wp-config.php
 sudo sed -i "s/<dbUserPassword>/$2/g" /var/www/wordpress/wp-config.php
 sudo sed -i "s/<backendIPAddress>/$3/g" /var/www/wordpress/wp-config.php
 sudo chown www-data:www-data /var/www/wordpress/wp-config.php
